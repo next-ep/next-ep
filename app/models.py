cd3caa824ddeaa8aa11390a0535a4ad631df48de
Ian <iancambreador@gmail.com>
2022-08-26 20:26:58 -0300

    Adicionando o db sqlite no gitignore

--- app/models.py
+++ app/models.py
@@ -10,9 +10,3 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
-
-class User2(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(15), unique=True)
-    email = db.Column(db.String(50), unique=True)
-    password = db.Column(db.String(80))
